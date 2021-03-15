04005d232b7186ba19c3c0dd4f822edeb131c6df
Teatoller <steven.otieno1@gmail.com>
2021-03-15 15:00:03 +0300

    (Customer): Implement Customer object
    - add Customer class
    - add getter and setter property
    - add static instance count property
    
    [Starts #003]

--- src/ACM/Customer.cs
+++ src/ACM/Customer.cs
@@ -1,5 +1,41 @@
 namespace ACM
 {
     public class Customer
-    {}
+    {
+        public int CustomerId { get; private set; }
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+        private string lastName;
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        private string fullName;
+        public string FullName
+        {
+
+            get
+            {
+                fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(LastName))
+                    {
+                        fullName += " ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+        public static int InstanceCount { get; set; }
+    }
 }
